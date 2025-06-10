d604d8ebc217aacb4cfb4a69b0744866898faca6
DanielGarcia-git <yadiaguila16@gmail.com>
2025-06-09 19:08:04 -0600

    Cambio

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -23,7 +23,7 @@ public class Conexion {
     public Connection abrirConexion(){
         try {
             // Cargar el driver JDBC ...
-            //Cambio xd
+            //Cambio xdddd
             System.setProperty("oracle.net.wallet_location", walletLocation);
             conexion = DriverManager.getConnection(url, "Admin", "EquipoK_Los3brutales@");
             System.out.println("Conexión exitosa");
